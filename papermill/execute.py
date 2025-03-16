b7d0dd835c1d794fb433e2f9c099c3384afa0ac4
Kentaro Aoki <kentaro.aoki@gmail.com>
2025-03-16 00:54:58 +0000

    Fix to continue even if errors exist.

--- papermill/execute.py
+++ papermill/execute.py
@@ -125,7 +125,7 @@ def execute_notebook(
                 )
 
             # Check for errors first (it saves on error before raising)
-            raise_for_execution_errors(nb, output_path)
+            # raise_for_execution_errors(nb, output_path)
 
         # Write final output in case the engine didn't write it on cell completion.
         write_ipynb(nb, output_path)
